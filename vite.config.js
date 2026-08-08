22293ba3a43be21efccf8b040b99e6cc46aba640
jkona0-code <jkona0@gmail.com>
2026-08-08 16:48:33 -0400

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hallyu-beauty/',
+  base: '/web_coreana/',
   plugins: [react()],
   server: {
     port: 5173,
